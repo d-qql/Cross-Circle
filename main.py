9b9a1000e8471684a8b961491af407f5595e8fc1
d-qql <petrov.dmitrii@phystehc.edu>
2020-10-15 16:24:13 +0300

    fixed bugs

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame
 pygame.init()
-HEIGHT = 5
+HEIGHT = 20
 WIDTH = HEIGHT
-SCALE = 100
+SCALE = 30
 
 
 RED = (255, 0, 0)
 ORANGE = (255, 200, 0)
 BLACK = (0, 0, 0)
 
-MATRIX = [[-1 for i in range(5)] for j in range(5)]
+MATRIX = [[-1 for i in range(WIDTH)] for j in range(HEIGHT)]
 print(MATRIX)
 
 screen = pygame.display.set_mode((HEIGHT*SCALE, WIDTH*SCALE))
@@ -28,49 +28,123 @@ def setCROSS(x, y):
     pygame.draw.line(screen, ORANGE, [x_-int(SCALE/3), y_+int(SCALE/3)], [x_+int(SCALE/3), y_-int(SCALE/3)], int(SCALE/20))
     pygame.draw.line(screen, ORANGE, [x_ - int(SCALE / 3), y_ - int(SCALE / 3)], [x_ + int(SCALE / 3), y_ + int(SCALE / 3)],int(SCALE/20))
 
+# def isWin():
+#     for i in range(HEIGHT):
+#         if MATRIX[i][0] != -1:
+#             k = MATRIX[i][0]
+#         else:
+#             continue
+#         S = True
+#         for j in range(WIDTH):
+#             if k != MATRIX[i][j]:
+#                 S = False
+#                 break
+#         if S:
+#             return ([i*SCALE+int(SCALE/2), int(SCALE/5)], [i*SCALE+int(SCALE/2), WIDTH*SCALE - int(SCALE/5)])
+#     for j in range(WIDTH):
+#         if MATRIX[0][j] != -1:
+#             k = MATRIX[0][j]
+#         else:
+#             continue
+#         S = True
+#         for i in range(HEIGHT):
+#             if k != MATRIX[i][j]:
+#                 S = False
+#                 break
+#         if S:
+#             return ([int(SCALE/5), j*SCALE+int(SCALE/2)], [HEIGHT*SCALE - int(SCALE/5), j*SCALE+int(SCALE/2)])
+#     k = MATRIX[0][0]
+#     S = True
+#     for i in range(WIDTH):
+#         if k != MATRIX[i][i] or MATRIX[i][i] == -1:
+#             S = False
+#             break
+#     if S:
+#         return ([int(SCALE/5), int(SCALE/5)], [WIDTH*SCALE - int(SCALE/5), HEIGHT*SCALE - int(SCALE/5)])
+#
+#     k = MATRIX[HEIGHT-1][0]
+#     S = True
+#     for i in range(WIDTH):
+#         if k != MATRIX[i][HEIGHT-i-1] or MATRIX[HEIGHT-i-1][i] == -1:
+#             S = False
+#             break
+#     if S:
+#         return ([int(SCALE/5), HEIGHT*SCALE - int(SCALE/5)], [WIDTH*SCALE - int(SCALE/5), int(SCALE/5)])
+#     return False
 def isWin():
     for i in range(HEIGHT):
-        if MATRIX[i][0] != -1:
-            k = MATRIX[i][0]
-        else:
-            continue
-        S = True
+        countx = 0
+        counto = 0
         for j in range(WIDTH):
-            if k != MATRIX[i][j]:
-                S = False
-                break
-        if S:
-            return ([i*SCALE+int(SCALE/2), int(SCALE/5)], [i*SCALE+int(SCALE/2), WIDTH*SCALE - int(SCALE/5)])
+            if(MATRIX[i][j] == 1):
+                countx += 1
+                counto = 0
+            if (MATRIX[i][j] == 0):
+                counto += 1
+                countx = 0
+            if(MATRIX[i][j] == -1):
+                countx = 0
+                counto = 0
+            if(countx == 5 or counto == 5):
+                return ([i * SCALE + int(SCALE / 2), (j+1)*SCALE - int(SCALE/5)],
+                        [i * SCALE + int(SCALE / 2), (j-4) * SCALE + int(SCALE / 5)])
     for j in range(WIDTH):
-        if MATRIX[0][j] != -1:
-            k = MATRIX[0][j]
-        else:
-            continue
-        S = True
+        countx = 0
+        counto = 0
         for i in range(HEIGHT):
-            if k != MATRIX[i][j]:
-                S = False
-                break
-        if S:
-            return ([int(SCALE/5), j*SCALE+int(SCALE/2)], [HEIGHT*SCALE - int(SCALE/5), j*SCALE+int(SCALE/2)])
-    k = MATRIX[0][0]
-    S = True
-    for i in range(WIDTH):
-        if k != MATRIX[i][i] or MATRIX[i][i] == -1:
-            S = False
-            break
-    if S:
-        return ([int(SCALE/5), int(SCALE/5)], [WIDTH*SCALE - int(SCALE/5), HEIGHT*SCALE - int(SCALE/5)])
+            if(MATRIX[i][j] == 1):
+                countx += 1
+                counto = 0
+            if (MATRIX[i][j] == 0):
+                counto += 1
+                countx = 0
+            if(MATRIX[i][j] == -1):
+                countx = 0
+                counto = 0
+            if(countx == 5 or counto == 5):
+                 return ([(i+1) * SCALE - int(SCALE/5), j*SCALE+int(SCALE/2)],
+                         [(i-4) * SCALE + int(SCALE/5), j*SCALE+int(SCALE/2)])
 
-    k = MATRIX[HEIGHT-1][0]
-    S = True
-    for i in range(WIDTH):
-        if k != MATRIX[i][HEIGHT-i-1] or MATRIX[HEIGHT-i-1][i] == -1:
-            S = False
-            break
-    if S:
-        return ([int(SCALE/5), HEIGHT*SCALE - int(SCALE/5)], [WIDTH*SCALE - int(SCALE/5), int(SCALE/5)])
+    for k in range(HEIGHT):
+        for g in range(WIDTH):
+            countx = 0
+            counto = 0
+            for i in range(5):
+                if (i+k >= HEIGHT or i+g >= WIDTH):
+                    continue
+                if (MATRIX[i+k][i+g] == 1):
+                    countx += 1
+                    counto = 0
+                if (MATRIX[i+k][i+g] == 0):
+                    counto += 1
+                    countx = 0
+                if (MATRIX[i+k][i+g] == -1):
+                    countx = 0
+                    counto = 0
+                if (countx == 5 or counto == 5):
+                    return ([k * SCALE + int(SCALE / 5),g * SCALE + int(SCALE / 5)],
+                            [(k+5) * SCALE - int(SCALE / 5), (g+5) * SCALE - int(SCALE / 5)])
+    for k in range(HEIGHT):
+        for g in range(WIDTH):
+            countx = 0
+            counto = 0
+            for i in range(5):
+                if (i+k >= HEIGHT or g-i < 0):
+                    continue
+                if (MATRIX[i+k][g-i] == 1):
+                    countx += 1
+                    counto = 0
+                if (MATRIX[i+k][g-i] == 0):
+                    counto += 1
+                    countx = 0
+                if (MATRIX[i+k][g-i] == -1):
+                    countx = 0
+                    counto = 0
+                if (countx == 5 or counto == 5):
+                    return ([k * SCALE + int(SCALE / 5),(g+1) * SCALE - int(SCALE / 5)],
+                            [(k+5) * SCALE - int(SCALE / 5), (g-4) * SCALE + int(SCALE / 5)])
     return False
+
 
 for i in range(0, HEIGHT):
     pygame.draw.line(screen, BLACK, [i * SCALE, 0], [i * SCALE, WIDTH * SCALE])
@@ -102,5 +176,9 @@ while running:
             p1, p2 = isWin()
             pygame.draw.line(screen, BLACK, p1, p2, int(SCALE/20))
             pygame.display.flip()
+            while running:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
         if event.type == pygame.QUIT:
             running = False
